80ffd4854751870d318a586e50371aba422fe91b
Maiara Andrade <maiara.andrade@usp.br>
2022-06-15 15:45:40 -0300

    mudanças em inscrições2

--- inscricao.php
+++ inscricao.php
@@ -9,7 +9,7 @@
   <div class="alert alert-info" role="warning">
 <p style="padding 30px" >
 As submissões de projetos para análise no 2º semestre de 2022 será entre 20/06 a 15/07, por meio do preenchimento do formulário em  <a href="docs/fichacea.doc" target="_blank" rel="noopener noreferrer" >DOC</a>
-    ou <a href="docs/fichacea.pdf" target="_blank" rel="noopener noreferrer">PDF</a> . Enviar para a secretária Alessandra por e-mail (arcanjo@ime.usp.br ou cea@ime.usp.br).
+    ou <a href="docs/fichacea.pdf" target="_blank" rel="noopener noreferrer">PDF</a> . Enviar para a secretária Alessandra por e-mail para cea@ime.usp.br.
 
 <br><br>
 As triagens serão realizadas de  26 de julho a 5 de agosto, de segunda-feira à sexta-feira, entre 9h30, 10h30 e 11h30 pela manhã e das 13h30, 14h30 e 15h30, por videoconferência (agendar horário com a secretária Alessandra por telefone (11-3091-6133) ou por e-mail (cea@ime.usp.br ou arcanjo@ime.usp.br).
