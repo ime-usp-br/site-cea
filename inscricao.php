0f8093d6e9a978a2821ad6994d3f4a9fd8e684c1
Maiara Andrade <maiara.andrade@usp.br>
2023-01-27 14:49:05 -0300

    modificação da home

--- inscricao.php
+++ inscricao.php
@@ -12,8 +12,7 @@ As inscrições para consultas <strong>(detalhes na aba Home)</strong> podem ser
 
 <p> As submissões de projetos <strong>(detalhes na aba Home)</strong> para análise no 1º semestre de 2023 ocorrerão de 17/01 a 17/02. As triagens de projetos serão realizadas de 23 de fevereiro a 9 de março por videoconferência (agendar horário por telefone (11-3091-6133) ou por e-mail (<a href = "mailto: cea@ime.usp.br">cea@ime.usp.br</a>)).
 
-<p>Tanto para consultas quanto para projetos deve ser preenchido o formulário em <a href="docs/fichacea.doc" target="_blank" rel="noopener noreferrer" >DOC</a>
-    ou <a href="docs/fichacea.pdf" target="_blank" rel="noopener noreferrer">PDF</a>. Enviar formulário e comprovante de pagamento da taxa de R$ 80,00 (oitenta reais) para a secretária Alessandra via e-mail para <a href = "mailto: cea@ime.usp.br">cea@ime.usp.br</a>.
+<p>Tanto para consultas quanto para projetos deve ser preenchido o formulário <a href="https://inscricao.cea.ime.usp.br/" target="_blank" rel="noopener noreferrer" >on-line</a>. O formulário contempla o envio do comprovante de pagamento da taxa de R$ 80,00 (oitenta reais).
 <br>
 
 </p>
@@ -28,10 +27,7 @@ As inscrições para consultas <strong>(detalhes na aba Home)</strong> podem ser
   <h3 style="text-align: justify;">Informações adicionais</h3>
   <p style="text-align: justify;"><strong>A - Procedimento para consultas</strong></p>
   <ol style="text-align: justify;">
-  <li>Preencher a ficha por meio dos formulários em formato <a href="docs/fichacea.doc" target="_blank" rel="noopener noreferrer">DOC</a> ou
-    <a href="docs/fichacea.pdf" target="_blank" rel="noopener noreferrer">PDF</a>
-    e enviar por e-mail ou entregar na secretaria do CEA (IME, Bloco A, Sala 214).</li>
-
+  <li>Preencher a ficha por meio do formulário <a href="https://inscricao.cea.ime.usp.br/" target="_blank" rel="noopener noreferrer">on-line</a>.</li>
   <li>Aguardar o contato da secretária do CEA para escolha da data da consulta. Detalhes sobre a forma de pagamento são encontrados no final desta página.</li>
   <li>Se houver interesse, marcar uma consulta de retorno para esclarecimentos finais que, na medida do possível,
     será realizada com os mesmos professores presentes na primeira consulta. Nesse retorno serão discutidas as sugestões apresentadas e/ou dada orientação
@@ -41,10 +37,7 @@ As inscrições para consultas <strong>(detalhes na aba Home)</strong> podem ser
   </ol>
   <p style="text-align: justify;"><strong>B - Procedimento para projetos</strong></p>
   <ol style="text-align: justify;">
-  <li>Preencher a ficha por meio dos formulários em formato
-    <a href="docs/fichacea.doc" target="_blank" rel="noopener noreferrer">DOC</a> ou
-    <a href="docs/fichacea.pdf" target="_blank" rel="noopener noreferrer">PDF</a> e
-    enviar por e-mail ou entregar na secretaria do CEA (IME, Bloco A, Sala 214).</li>
+  <li>Preencher a ficha por meio do formulário <a href="https://inscricao.cea.ime.usp.br/" target="_blank" rel="noopener noreferrer">on-line</a>.</li>
   <li>Aguardar o contato da secretária do CEA para escolha da data da triagem (fevereiro/março ou julho/agosto).  Detalhes sobre a forma de pagamento são encontrados no final desta página. Em caso de mestrados ou doutorados, comparecer com o(a) orientador(a).</li>
   <li>Aguardar resposta do CEA sobre a possibilidade ou não da realização do projeto. Esta resposta geralmente é dada no prazo máximo de um mês após a triagem.</li>
   <li>Caso o projeto seja selecionado, pagar taxa adicional de R$ 200,00 (duzentos reais).</li>
