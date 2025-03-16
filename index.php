484c68274c7f98acf08826b851cb333eb808d8fb
enzo.ikeda <enzo.ikeda@usp.br>
2025-03-16 10:33:24 -0300

    Atualização das palestras

--- index.php
+++ index.php
@@ -52,20 +52,6 @@ ALERTA PARA DATA DAS INSCRIÇÕES
   PROGRAMAÇÃO DAS PALESTRAS
   <div class="alert alert-info" role="warning" style="background-color: yellow; color: black;">
     <!---<p><b>No momento, não há palestras planejadas.</b></p>--->
-  <p><b>Palestra: "Muito além da técnica: habilidades essenciais para estatísticos"</b></p>
-  <ul>
-  <li style="text-align: justify;"><b>Palestrante</b>: Magno Tairone de Freitas Severino - Pefisa Soluções Financeiras e Insper</li>
-  <li style="text-align: justify;"><b>Data</b>: 11/03/2025</li>
-  <li style="text-align: justify;"><b>Horário</b>: 17h00</li>
-  <li style="text-align: justify;"><b>Local</b>: IME-USP, Sala B16</li>
-  </ul>
-  <p><b>Palestra: "Modelos mistos segmentados com ponto de mudança aleatório"</b></p>
-  <ul>
-  <li style="text-align: justify;"><b>Palestrante</b>: Paulo Henrique Dourado da Silva - Banco do Brasil - IME-USP</li>
-  <li style="text-align: justify;"><b>Data</b>: 13/03/2025</li>
-  <li style="text-align: justify;"><b>Horário</b>: 16h00</li>
-  <li style="text-align: justify;"><b>Local</b>: IME-USP, Sala B16</li>
-  </ul>
   <p><b>Palestra: "Métodos Bayesianos para cálculo de tamanho amostral em ensaios clínicos randomizados em cluster"</b></p>
   <ul>
   <li style="text-align: justify;"><b>Palestrante</b>: Lucas Petri Damiani - Brazilian Clinical Research Institute - BCRI e IME-USP</li>
